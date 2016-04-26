e90654c6a9636e95f90984428f50257bd850e06e
Koray Al <admin@koray.al>
2016-04-27 00:43:05 +0300

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+    ( feedList,
+      FeedList(..),
+      FeedItem(..)
+    ) where
+
+import Data.Aeson
+import Data.Time.Clock (UTCTime)
+import Data.Time.Clock.POSIX
+import qualified Data.ByteString.Lazy as B
+
+data FeedItem = FeedItem
+    { title :: String
+    , engagement :: Maybe Integer
+    , published :: UTCTime
+    , crawled :: UTCTime
+    } deriving ((((Show))))
+data FeedList = FeedList { items :: [FeedItem]} deriving (Show)
+
+instance FromJSON FeedItem where
+  parseJSON = withObject "Feed Item" $ \o ->
+                FeedItem <$> o .: "title"
+                         <*> o .:? "engagement"
+                         <*> (posixSecondsToUTCTime . (/ 1000) . fromInteger <$> o .: "published")
+                         <*> (posixSecondsToUTCTime . (/ 1000) . fromInteger <$> o .: "crawled")
+
+
+instance FromJSON FeedList where
+  parseJSON = withObject "Feed List" $ \o ->
+                FeedList <$> o .: "items"
+
+feedList :: FilePath -> IO (Either String FeedList)
+feedList p = eitherDecode <$> B.readFile p
