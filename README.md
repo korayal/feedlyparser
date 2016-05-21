e1c1a102fac8bd164acf7aeb9f6a9215a98ef997
Koray Al <admin@koray.al>
2016-05-21 21:43:38 +0300

    Added all details about feeds and updated README

--- README.md
+++ README.md
@@ -8,10 +8,12 @@ Next target will be to fetch JSON directly from API, maybe persist it somewhere 
 
 ----
 
-to test it out, run `stack ghci` and then you can play with your json output:
+to test it out, 
+run `stack exec fp -- "hn.json"`
+or in `stack ghci` and then you can play with your json output:
 
 ``` haskell
-*Main Lib> Right x <- feedList "/home/korayal/Desktop/hn.json"
+*Main Lib> Right x <- feedList "hn.json" 
 *Main Lib> head $ items x
-FeedItem {title = "Using Caffe models in Tensorflow", engagement = 10, published = 2015-11-20 18:51:04 UTC, crawled = 2015-11-20 19:39:38.545 UTC}
+FeedItem {id = "2tM1DedEr6eqgdqJ8X4sP+CS3BVG/bkK4hS45ZnKnRQ=_1512668c2b1:11fa642:48f117c4", originId = "https://github.com/ethereon/caffe-tensorflow", fingerprint = "de117aa5", title = "Using Caffe models in Tensorflow", published = 2015-11-20 18:51:04 UTC, crawled = 2015-11-20 19:39:38.545 UTC, alternate = [FeedAlternate {href = "https://github.com/ethereon/caffe-tensorflow", _type = "text/html"}], origin = FeedOrigin {streamId = "feed/https://news.ycombinator.com/rss", foTitle = "Hacker News", htmlUrl = "https://news.ycombinator.com/"}, summary = FeedSummary {content = "<a href=\"https://news.ycombinator.com/item?id=10603037\">Comments</a>", direction = "ltr"}, visual = Just (FeedVisual {url = "https://avatars1.githubusercontent.com/u/337985?v=3&s=400", width = 400, height = 400, processor = "feedly-nikon-v3.1", contentType = "image/png"}), unread = True, categories = [FeedCategory {fcid = "user/e0bfad67-abba-4764-be3a-a44cd3ede219/category/g\252nl\252k.takip", fclabel = "g\252nl\252k.takip"}], engagement = 10, engagementRate = 0}
 ```
--- src/Lib.hs
+++ src/Lib.hs
@@ -11,23 +11,103 @@ import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX
 import qualified Data.ByteString.Lazy as B
 
+data FeedAlternate = FeedAlternate
+  { href :: String
+  , _type :: String
+  } deriving (Show)
+
+newtype FeedAlternateList = FeedAlternateList { alternateList :: [FeedAlternate] } deriving (Show)
+
+instance FromJSON FeedAlternate where
+  parseJSON = withObject "Feed Alternate" $ \o ->
+                FeedAlternate <$> o .: "href"
+                              <*> o .: "type"
+
+data FeedOrigin = FeedOrigin
+  { streamId :: String
+  , foTitle :: String
+  , htmlUrl :: String
+  } deriving (Show)
+
+instance FromJSON FeedOrigin where
+  parseJSON = withObject "Feed Origin" $ \o ->
+                FeedOrigin <$> o .: "streamId"
+                           <*> o .: "title"
+                           <*> o .: "htmlUrl"
+
+data FeedSummary = FeedSummary
+  { content :: String
+  , direction :: String
+  } deriving (Show)
+
+instance FromJSON FeedSummary where
+  parseJSON = withObject "Feed Summary" $ \o ->
+                FeedSummary <$> o .: "content"
+                            <*> o .: "direction"
+
+data FeedVisual = FeedVisual
+  { url :: String
+  , width :: Integer
+  , height :: Integer
+  , processor :: String
+  , contentType :: String
+  } deriving (Show)
+
+instance FromJSON FeedVisual where
+  parseJSON = withObject "Feed Visual" $ \o ->
+                FeedVisual <$> o .: "url"
+                           <*> (fromInteger <$> o .:? "width" .!= 0)
+                           <*> (fromInteger <$> o .:? "height" .!= 0)
+                           <*> (o .:? "processor" .!= "")
+                           <*> (o .:? "contentType" .!= "")
+
+data FeedCategory = FeedCategory
+  { fcid :: String
+  , fclabel :: String
+  } deriving (Show)
+
+newtype FeedCategoryList = FeedCategoryList { categoryList :: [FeedCategory] } deriving (Show)
+
+instance FromJSON FeedCategory where
+  parseJSON = withObject "Feed Category" $ \o ->
+                FeedCategory <$> o .: "id"
+                             <*> o .: "label"
+
 data FeedItem = FeedItem
-    { title :: String
-    , engagement :: Integer
+    { id :: String
+    , originId :: String
+    , fingerprint :: String
+    , title :: String
     , published :: UTCTime
     , crawled :: UTCTime
-    } deriving ((((Show))))
-
-data FeedList = FeedList { items :: [FeedItem]} deriving (Show)
+    , alternate :: [FeedAlternate]
+    , origin :: FeedOrigin
+    , summary :: FeedSummary
+    , visual :: Maybe FeedVisual
+    , unread :: Bool
+    , categories :: [FeedCategory]
+    , engagement :: Integer
+    , engagementRate :: Integer
+    } deriving (Show)
 
 instance FromJSON FeedItem where
   parseJSON = withObject "Feed Item" $ \o ->
-                FeedItem <$> o .: "title"
-                         -- set engagement to 0 if empty
-                         <*> o .:? "engagement" .!= 0
+                FeedItem <$> o .: "id"
+                         <*> o .: "originId"
+                         <*> o .: "fingerprint"
+                         <*> o .: "title"
                          <*> (posixSecondsToUTCTime . (/ 1000) . fromInteger <$> o .: "published")
                          <*> (posixSecondsToUTCTime . (/ 1000) . fromInteger <$> o .: "crawled")
+                         <*> (alternateList . FeedAlternateList <$> o .: "alternate")
+                         <*> o .: "origin"
+                         <*> o .: "summary"
+                         <*> (o .:? "visual")
+                         <*> o .: "unread"
+                         <*> (categoryList . FeedCategoryList <$> o .: "categories")
+                         <*> o .:? "engagement" .!= 0
+                         <*> o .:? "engagementRate" .!= 0
 
+data FeedList = FeedList { items :: [FeedItem]} deriving (Show)
 
 instance FromJSON FeedList where
   parseJSON = withObject "Feed List" $ \o ->
